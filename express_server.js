6d6e4d494a0036a64a9ae7d83798c86ac2a9d568
Curtis Platt <curtisplatt@hotmail.com>
2023-08-09 13:47:25 -0400

    Cookies in Express

--- express_server.js
+++ express_server.js
@@ -82,3 +82,9 @@ app.post("/urls/:id", (req, res) => {
   console.log(urlDatabase);
   res.redirect("/urls");
 });
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
